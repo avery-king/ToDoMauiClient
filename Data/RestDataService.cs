aa0c21202cf3b290304e258f86ac7b584bce20fd
Avery King <averyking345@gmail.com>
2024-05-28 10:19:10 -0400

    data service complete

--- Data/RestDataService.cs
+++ Data/RestDataService.cs
@@ -1,72 +1,88 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using ToDoMauiClient.Models;
 
-namespace ToDoMauiClient.Data;
-
-public class RestDataService : IRestDataService
+namespace ToDoMauiClient.Data
 {
-    private readonly HttpClient _httpClient;
-    private readonly string _baseAddress;
-    private readonly string _url;
-    private readonly JsonSerializerOptions _jsonSerializerOptions;
-    
-    public RestDataService()
+    public class RestDataService : IRestDataService
     {
-        _httpClient = new HttpClient();
-        _baseAddress = DeviceInfo.Platform == DevicePlatform.Android
-            ? "http://10.0.2.2:5209"
-            : "https://localhost:7209";
-        _url = $"{_baseAddress}/api";
-        _jsonSerializerOptions = new JsonSerializerOptions()
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-        };
-    }
+        private readonly HttpClient _httpClient;
+        private readonly string _baseAddress;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
 
-    public async Task<List<ToDo>?> GetAllItemsAsync()
-    {
-        var toDoList = new List<ToDo>();
-
-        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+        public RestDataService()
         {
-            Debug.WriteLine("----> No internet access");
-            return toDoList;
-        }
-
-        try
-        {
-            var response = await _httpClient.GetAsync($"{_url}/api/todo");
-            if (response.IsSuccessStatusCode)
+            _httpClient = new HttpClient();
+            _baseAddress = DeviceInfo.Platform == DevicePlatform.Android
+                ? "http://10.0.2.2:5209"
+                : "https://localhost:7209";
+            _httpClient.BaseAddress = new Uri(_baseAddress);
+            _jsonSerializerOptions = new JsonSerializerOptions()
             {
-                var content = await response.Content.ReadAsStringAsync();
-                toDoList = JsonSerializer.Deserialize<List<ToDo>>(content, _jsonSerializerOptions);
-            }
-            else
-            {
-                Debug.WriteLine("----> Unsuccessful HTTP  code");
-            }
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
         }
-        catch (Exception ex)
+
+        private async Task<T> SendRequest<T>(HttpMethod method, string endpoint, object? requestBody = null)
         {
-            Debug.WriteLine($"Exception: {ex.Message}");
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("----> No internet access");
+                return default!;
+            }
+
+            try
+            {
+                var request = new HttpRequestMessage(method, endpoint);
+                if (requestBody != null)
+                {
+                    var jsonRequestBody = JsonSerializer.Serialize(requestBody, _jsonSerializerOptions);
+                    request.Content = new StringContent(jsonRequestBody, Encoding.UTF8, "application/json");
+                }
+
+                var response = await _httpClient.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<T>(content, _jsonSerializerOptions)!;
+                }
+                else
+                {
+                    Debug.WriteLine("----> Unsuccessful HTTP code");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception: {ex.Message}");
+            }
+
+            return default!;
         }
 
-        return toDoList;
-    } 
+        public async Task<List<ToDo>?> GetAllItemsAsync()
+        {
+            return await SendRequest<List<ToDo>>(HttpMethod.Get, "/api/todo");
+        }
 
-    public Task AddItemAsync(ToDo toDo)
-    {
-        throw new NotImplementedException();
-    }
+        public async Task AddItemAsync(ToDo toDo)
+        {
+            await SendRequest<object>(HttpMethod.Post, "/api/todo", toDo);
+        }
 
-    public Task UpdateItemAsync(ToDo toDo)
-    {
-        throw new NotImplementedException();
-    }
+        public async Task UpdateItemAsync(ToDo toDo)
+        {
+            await SendRequest<object>(HttpMethod.Put, $"/api/todo/{toDo.Id}", toDo);
+        }
 
-    public Task DeleteItemAsync(int id)
-    {
-        throw new NotImplementedException();
+        public async Task DeleteItemAsync(int id)
+        {
+            await SendRequest<object>(HttpMethod.Delete, $"/api/todo/{id}");
+        }
     }
 }
